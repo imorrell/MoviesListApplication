bf3fb4229a932e5d861928b34be010a96f604eff
Ivoire Morrell <imorrell@ltu.edu>
2019-10-24 22:50:36 -0400

    Movies list application

---
+++ MoviesList/Movie.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesList
+{
+   public class Movie
+    {
+
+        #region fields
+        private string title;
+        private string category;
+        #endregion
+
+        #region properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; } 
+        }
+        #endregion
+
+        #region methods
+        public Movie()
+        {
+
+
+        }
+
+        public Movie(string movieTitle, string movieCategory)
+        {
+            title = movieTitle;
+            category = movieCategory;
+        }
+
+        private static List<string> MovieCategory(List<Movie> list)
+        {
+            List<String> movieCategory = new List<string>();
+
+            foreach (Movie movie in list)
+            {
+                if (!movieCategory.Contains(movie.Category))
+                {
+                    movieCategory.Add(movie.Category);
+                }
+            }
+
+            return movieCategory;
+        }
+
+        public static void PrintMovieCategories(List<Movie> movieList)
+        {
+            List<string> movieCategory = Movie.MovieCategory(movieList);
+
+            for (int i = 0; i < movieCategory.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {movieCategory[i]}");
+            }
+
+        }
+
+        //The print out movie by category method accepts a movies list and integer. 
+        //It will print out movies based off number selection
+        public static List<string> PrintMovieByCategory(List<Movie> movieList, int selection)
+        {
+            List<string> moviesCategory = new List<string>();
+
+            //use switch statement to process the users input
+            switch (selection)
+            {
+                //user chooses action
+                case 1:
+                    {
+                        Console.WriteLine("Action movies");
+                        //print out all the movies in the list that are action
+                        for (int i = 0; i < movieList.Count; i++)
+                        {
+                            if (movieList[i].Category.Equals("action", StringComparison.OrdinalIgnoreCase))
+                            {
+                                //print out action movies
+                                moviesCategory.Add(movieList[i].Title);
+                            }
+                        }
+                        return moviesCategory;
+                    }
+                //user chooses crime
+                case 2:
+                    {
+                        Console.WriteLine("Crime movies");
+                        //print out all the movies in the list that are crime
+                        for (int i = 0; i < movieList.Count; i++)
+                        {
+                            if (movieList[i].Category.Equals("crime", StringComparison.OrdinalIgnoreCase))
+                            {
+                                //print out action movies
+                                moviesCategory.Add(movieList[i].Title);
+                            }
+                        }
+
+                        return moviesCategory;
+                    }
+
+                //user chooses mystery
+                case 3:
+                    {
+                        Console.WriteLine("Mystery movies");
+                        //print out all the movies in the list that are mystery
+                        for (int i = 0; i < movieList.Count; i++)
+                        {
+                            if (movieList[i].Category.Equals("mystery", StringComparison.OrdinalIgnoreCase))
+                            {
+                                //print out action movies
+                                moviesCategory.Add(movieList[i].Title);
+                            }
+                        }
+                        return moviesCategory;
+                    }
+
+                //user chooses biography
+                case 4:
+                    {
+                        Console.WriteLine("Biography movies");
+                        //print out all the movies in the list that are mystery
+                        for (int i = 0; i < movieList.Count; i++)
+                        {
+                            if (movieList[i].Category.Equals("biography", StringComparison.OrdinalIgnoreCase))
+                            {
+                                //print out action movies
+                                moviesCategory.Add(movieList[i].Title);
+                            }
+                        }
+                        return moviesCategory;
+                    }
+                default:
+                    {
+                        //movie category not found
+                        Console.WriteLine("Movie catergory not found. \n");
+                        return moviesCategory;
+                    }
+                    
+            }
+        }
+        #endregion
+        
+    }
+}
---
+++ MoviesList/Program.cs
@@ -0,0 +1,156 @@
+﻿/*Program: Movies list
+ *Programmer: Ivoire Morrell 
+ *Date: 10-24-19 
+ *Description: This proram creates a list of movie 
+ *objects and allows the user to print out movies by category
+ * 
+ */
+using System;
+using System.Collections.Generic;
+
+namespace MoviesList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initalize variables
+            bool ok = true;
+            int selection;
+            List<string> categoryMovies = new List<string>();
+
+            //initalize list of movies with at least 10 movies
+            List<Movie> movieList = new List<Movie>
+            {
+                new Movie("The Dark Knight", "Action"),
+                new Movie("The Departed", "Crime"),
+                new Movie("Shutter Island", "Mystery"),
+                new Movie("12 Years A Slave", "Biography"),
+                new Movie("Kill Bill V1", "Action"),
+                new Movie("Goodfellas", "Biography"),
+                new Movie("Wolf on Wall Street", "Biography"),
+                new Movie("Pirates of the Carabean: Dead Mans Chest", "Action"),
+                new Movie("Scarface", "Crime"),
+                new Movie("Traning Day", "Crime"),
+
+            };
+
+            //welcome the user to the program
+            Console.WriteLine("Welcome to the movie application! \n");
+
+            Console.WriteLine("There're are 10 movies in this list. \n");
+
+            //wrap code in while loop to determine if the user wants to slect a different category
+            while (ok)
+            {
+
+
+                //Display the category list
+                Movie.PrintMovieCategories(movieList);
+
+                Console.WriteLine();
+
+                //call the ParseString method to convert users 
+                //string input to int and check if number is valid
+                selection = ValidateRange("Which category would you like to choose from? \n", 1, movieList.Count - 1);
+
+                //call movies by category method
+                categoryMovies = Movie.PrintMovieByCategory(movieList, selection);
+
+                Console.WriteLine();
+
+                //sort category movies
+                categoryMovies.Sort();
+
+                //print out the movies
+                foreach (string movie in categoryMovies)
+                {
+                    Console.WriteLine(movie);
+                }
+
+                Console.WriteLine();
+                //ask user if they would like to go again
+                ok = GetContinue("Continue (y/n)\n");
+                Console.WriteLine();
+            }
+}
+        public static int ValidateRange(string message, int min, int max)
+        {
+            int number = ParseString(message);
+
+            if (number >= min && number < max)
+            {
+                return number;
+            }
+            else
+            {
+                //This student does not exist
+                Console.WriteLine("This category does not exist.\n");
+                return ValidateRange(message, min, max);
+            }
+        }
+        public static int ParseString(string message)
+        {
+            try
+            {
+                string input = GetUserInput(message);
+                int number = int.Parse(input);
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bad input. We need a number \n");
+                return ParseString(message);
+            }
+
+        }
+        public static string GetUserInput(string message)
+        {
+            string input;
+
+            Console.WriteLine(message);
+
+            input = Console.ReadLine();
+
+            return input;
+        }
+        public static bool GetContinue(string message)
+        {
+            //create variables
+            String choice;
+            bool ok = true;
+            bool result = true;
+
+            //create while loop to determine if user wants to continue
+            while (ok)
+            {
+                Console.WriteLine(message);
+
+                //retrieve user input
+                choice = Console.ReadLine();
+
+                //determine the users input and return corresponding message
+                if (choice.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    //user wants to continue. exit the while loop and return true
+                    ok = false;
+                    result = true;
+                }
+                else if (choice.Equals("n", StringComparison.OrdinalIgnoreCase))
+                {
+                    //user does not want to continue
+                    ok = false;
+                    result = false;
+                }
+                else
+                {
+                    //user did not enter y or n
+                    Console.WriteLine("Error! Please enter Y or N. Please enter correct input \n");
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
